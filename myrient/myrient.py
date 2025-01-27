9f59af9543a678944022a55126c158a9a267ca07
HWXLR8 <68k@segv.moe>
2025-01-27 00:07:06 -0500

    update myrient dirs

--- myrient/myrient.py
+++ myrient/myrient.py
@@ -4,22 +4,44 @@ import os
 import subprocess
 import sys
 
-consoles = {
-    "NES" : [
-        "/files/No-Intro/Nintendo - Nintendo Entertainment System (Headered)/",
-        "/files/No-Intro/Nintendo - Nintendo Entertainment System (Headered) (Private)/",
-    ],
-
-    "SNES" : [
-        "/files/No-Intro/Nintendo - Super Nintendo Entertainment System/",
-        "/files/No-Intro/Nintendo - Super Nintendo Entertainment System (Private)/",
-    ],
-
-    "GENESIS" : [
-        "/files/No-Intro/Sega - Mega Drive - Genesis/",
-        "/files/No-Intro/Sega - Mega Drive - Genesis (Private)/",
-    ],
-}
+paths = [
+    # NES
+    "/files/No-Intro/Nintendo - Nintendo Entertainment System (Headered)/",
+    "/files/No-Intro/Nintendo - Nintendo Entertainment System (Headered) (Private)/",
+    # SNES
+    "/files/No-Intro/Nintendo - Super Nintendo Entertainment System/",
+    "/files/No-Intro/Nintendo - Super Nintendo Entertainment System (Private)/",
+    # GENESIS
+    "/files/No-Intro/Sega - Mega Drive - Genesis/",
+    "/files/No-Intro/Sega - Mega Drive - Genesis (Private)/",
+    # GB
+    "/files/No-Intro/Nintendo - Game Boy/",
+    "/files/No-Intro/Nintendo - Game Boy (Private)/",
+    # GBC
+    "/files/No-Intro/Nintendo - Game Boy Color/",
+    "/files/No-Intro/Nintendo - Game Boy Color (Private)/",
+    # GBA
+    "/files/No-Intro/Nintendo - Game Boy Advance/",
+    "/files/No-Intro/Nintendo - Game Boy Advance (Multiboot)/",
+    "/files/No-Intro/Nintendo - Game Boy Advance (Play-Yan)/",
+    "/files/No-Intro/Nintendo - Game Boy Advance (Private)/",
+    "/files/No-Intro/Nintendo - Game Boy Advance (Video)/",
+    "/files/No-Intro/Nintendo - Game Boy Advance (e-Reader)/",
+    # N64
+    "/files/No-Intro/Nintendo - Nintendo 64 (BigEndian)/",
+    "/files/No-Intro/Nintendo - Nintendo 64 (BigEndian) (Private)/",
+    "/files/No-Intro/Nintendo - Nintendo 64DD/",
+    # NDS
+    "/files/No-Intro/Nintendo - Nintendo DS (DSvision SD cards)/",
+    "/files/No-Intro/Nintendo - Nintendo DS (Decrypted)/",
+    "/files/No-Intro/Nintendo - Nintendo DS (Decrypted) (Private)/",
+    "/files/No-Intro/Nintendo - Nintendo DS (Download Play)/",
+    "/files/No-Intro/Nintendo - Nintendo DSi (Decrypted)/",
+    "/files/No-Intro/Nintendo - Nintendo DSi (Digital)/",
+    "/files/No-Intro/Nintendo - Nintendo DSi (Digital) (CDN) (Decrypted)/",
+    # misc
+    "/files/No-Intro/Nintendo - Misc/",
+]
 
 if len(sys.argv) < 2:
     print("Usage: myrient [ROM_PATH]")
@@ -28,11 +50,11 @@ if len(sys.argv) < 2:
 ROM_PATH = sys.argv[1]
 os.chdir(ROM_PATH)
 
-for console, variations in consoles.items():
-    print(f"creating directory {console}")
-    os.makedirs(console, exist_ok=True)
-    os.chdir(console)
-    for v in variations:
-        print(f'copying {v}')
-        subprocess.Popen(f'rclone copy "myrient:{v}" . --progress --http-no-head', shell=True)
+for p in paths:
+    d = os.path.basename(os.path.normpath(p))
+    os.makedirs(d, exist_ok=True)
+    os.chdir(d)
+    print(f'copying {d}')
+    p = subprocess.Popen(f'rclone copy "myrient:{p}" . --progress --http-no-head --size-only', shell=True)
+    p.wait()
     os.chdir(ROM_PATH)
